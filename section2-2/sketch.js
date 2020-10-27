5b09014aaeb36885fbbc820fedcc3f2d5a2e2f79
takeshi-nishida <takeshi-nishida@users.noreply.github.com>
2020-10-27 11:00:24 +0900

    ヒントを作った

--- section2-2/sketch.js
+++ section2-2/sketch.js
@@ -9,7 +9,10 @@ function setup() {
   background(196);
   for(let i = 0; i < 9; i++){
     // 変えるのはここから
-    if(i % 2 == 0){
+    if(i % 3  == 0){
+      strokeWeight(4);
+    }
+    else if(i % 3 == 1){
       strokeWeight(2);
     }
     else{
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,22 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+
+      if(/* 黒マスになる条件：ヒントビデオ見てね */){ 
+        fill(128); // fill(0) だと真っ黒になって黒丸が見えなくなってしまう
+      }
+      else{
+        fill(255); // white
+      }
+      rect(size * i, size * j, size, size);
+
+      if(/* 赤い丸を描く条件は「黒マス && 最初の三行かどうかをjを使って判定」 */){ 
+        ellipse(???, ???, size, size);
+      }
+      if(/* 黒い丸も同様に考えてみましょう */){
+        ellipse(???, ???, size, size);
+      }
+
     }
   }
 }
